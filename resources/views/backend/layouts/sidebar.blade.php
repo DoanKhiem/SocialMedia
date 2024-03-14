f7b87b478d859b31dde0bc358fd9ee22fd3c9000
khiemdev <khiembi161219@gmail.com>
2024-03-14 14:42:26 +0700

    menu items

---
+++ resources/views/backend/layouts/sidebar.blade.php
@@ -0,0 +1,272 @@
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+    <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+        <div class="sidebar-brand-icon rotate-n-15">
+            <i class="fas fa-laugh-wink"></i>
+        </div>
+        <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
+    </a>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider my-0">
+
+    <!-- Nav Item - Dashboard -->
+    <li class="nav-item active">
+        <a class="nav-link" href="index.html">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard</span></a>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+    <!-- Heading -->
+{{--    <div class="sidebar-heading">--}}
+{{--        Interface--}}
+{{--    </div>--}}
+
+    <!-- Nav Item - Banner Management Menu -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+{{--            <i class="fas fa-fw fa-cog"></i>--}}
+            <i class="fa-solid fa-image"></i>
+            <span>Banner Management</span>
+        </a>
+        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+{{--                <h6 class="collapse-header">Custom Components:</h6>--}}
+                <a class="collapse-item" href="buttons.html">Buttons</a>
+                <a class="collapse-item" href="cards.html">Cards</a>
+            </div>
+        </div>
+    </li>
+
+    <!-- Nav Item - Category Management Menu -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
+{{--            <i class="fas fa-fw fa-wrench"></i>--}}
+            <i class="fa-solid fa-sitemap"></i>
+            <span>Category Management</span>
+        </a>
+        <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+{{--                <h6 class="collapse-header">Custom Utilities:</h6>--}}
+                <a class="collapse-item" href="utilities-color.html">Colors</a>
+                <a class="collapse-item" href="utilities-border.html">Borders</a>
+                <a class="collapse-item" href="utilities-animation.html">Animations</a>
+                <a class="collapse-item" href="utilities-other.html">Other</a>
+            </div>
+        </div>
+    </li>
+
+    <!-- Nav Item - Products Management Menu -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+{{--            <i class="fas fa-fw fa-cog"></i>--}}
+            <i class="fa-solid fa-briefcase"></i>
+            <span>Products Management</span>
+        </a>
+        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                {{--                <h6 class="collapse-header">Custom Components:</h6>--}}
+                <a class="collapse-item" href="buttons.html">Buttons</a>
+                <a class="collapse-item" href="cards.html">Cards</a>
+            </div>
+        </div>
+    </li>
+
+    <!-- Nav Item - Carts Management Menu -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+{{--            <i class="fas fa-fw fa-cog"></i>--}}
+            <i class="fa-solid fa-cart-plus"></i>
+            <span>Carts Management</span>
+        </a>
+        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                {{--                <h6 class="collapse-header">Custom Components:</h6>--}}
+                <a class="collapse-item" href="buttons.html">Buttons</a>
+                <a class="collapse-item" href="cards.html">Cards</a>
+            </div>
+        </div>
+    </li>
+
+    <!-- Nav Item - Orders Management -->
+    <li class="nav-item">
+        <a class="nav-link" href="charts.html">
+{{--            <i class="fas fa-fw fa-chart-area"></i>--}}
+            <i class="fa-solid fa-layer-group"></i>
+            <span>Orders Management</span></a>
+    </li>
+
+    <!-- Nav Item - Post Category Menu -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+{{--            <i class="fas fa-fw fa-cog"></i>--}}
+            <i class="fa-solid fa-sitemap"></i>
+            <span>Post Category</span>
+        </a>
+        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                {{--                <h6 class="collapse-header">Custom Components:</h6>--}}
+                <a class="collapse-item" href="buttons.html">Buttons</a>
+                <a class="collapse-item" href="cards.html">Cards</a>
+            </div>
+        </div>
+    </li>
+
+    <!-- Nav Item - Post Tag Menu -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+{{--            <i class="fas fa-fw fa-cog"></i>--}}
+            <i class="fa-solid fa-tags"></i>
+            <span>Post Tag</span>
+        </a>
+        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                {{--                <h6 class="collapse-header">Custom Components:</h6>--}}
+                <a class="collapse-item" href="buttons.html">Buttons</a>
+                <a class="collapse-item" href="cards.html">Cards</a>
+            </div>
+        </div>
+    </li>
+
+    <!-- Nav Item - Post Management Menu -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+{{--            <i class="fas fa-fw fa-cog"></i>--}}
+            <i class="fa-solid fa-newspaper"></i>
+            <span>Post Management</span>
+        </a>
+        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                {{--                <h6 class="collapse-header">Custom Components:</h6>--}}
+                <a class="collapse-item" href="buttons.html">Buttons</a>
+                <a class="collapse-item" href="cards.html">Cards</a>
+            </div>
+        </div>
+    </li>
+
+    <!-- Nav Item - Review Management Menu -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+{{--            <i class="fas fa-fw fa-cog"></i>--}}
+            <i class="fa-solid fa-star"></i>
+            <span>Review Management</span>
+        </a>
+        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                {{--                <h6 class="collapse-header">Custom Components:</h6>--}}
+                <a class="collapse-item" href="buttons.html">Buttons</a>
+                <a class="collapse-item" href="cards.html">Cards</a>
+            </div>
+        </div>
+    </li>
+
+    <!-- Nav Item - Coupon Management Menu -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+            {{--            <i class="fas fa-fw fa-cog"></i>--}}
+            <i class="fa-solid fa-ticket"></i>
+            <span>Coupon Management</span>
+        </a>
+        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                {{--                <h6 class="collapse-header">Custom Components:</h6>--}}
+                <a class="collapse-item" href="buttons.html">Buttons</a>
+                <a class="collapse-item" href="cards.html">Cards</a>
+            </div>
+        </div>
+    </li>
+
+    <!-- Nav Item - Users Management Menu -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+            {{--            <i class="fas fa-fw fa-cog"></i>--}}
+            <i class="fa-solid fa-users"></i>
+            <span>Users Management</span>
+        </a>
+        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                {{--                <h6 class="collapse-header">Custom Components:</h6>--}}
+                <a class="collapse-item" href="buttons.html">Buttons</a>
+                <a class="collapse-item" href="cards.html">Cards</a>
+            </div>
+        </div>
+    </li>
+
+    <!-- Nav Item - Comments Management Menu -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+            {{--            <i class="fas fa-fw fa-cog"></i>--}}
+            <i class="fa-solid fa-comments"></i>
+            <span>Comments Management</span>
+        </a>
+        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                {{--                <h6 class="collapse-header">Custom Components:</h6>--}}
+                <a class="collapse-item" href="buttons.html">Buttons</a>
+                <a class="collapse-item" href="cards.html">Cards</a>
+            </div>
+        </div>
+    </li>
+
+    <!-- Nav Item - Settings -->
+    <li class="nav-item">
+        <a class="nav-link" href="charts.html">
+            <i class="fa-solid fa-gear"></i>
+            <span>Settings</span></a>
+    </li>
+
+    <!-- Divider -->
+{{--    <hr class="sidebar-divider">--}}
+
+    <!-- Heading -->
+{{--    <div class="sidebar-heading">--}}
+{{--        Addons--}}
+{{--    </div>--}}
+
+    <!-- Nav Item - Pages Collapse Menu -->
+{{--    <li class="nav-item">--}}
+{{--        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">--}}
+{{--            <i class="fas fa-fw fa-folder"></i>--}}
+{{--            <span>Pages</span>--}}
+{{--        </a>--}}
+{{--        <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">--}}
+{{--            <div class="bg-white py-2 collapse-inner rounded">--}}
+{{--                <h6 class="collapse-header">Login Screens:</h6>--}}
+{{--                <a class="collapse-item" href="login.html">Login</a>--}}
+{{--                <a class="collapse-item" href="register.html">Register</a>--}}
+{{--                <a class="collapse-item" href="forgot-password.html">Forgot Password</a>--}}
+{{--                <div class="collapse-divider"></div>--}}
+{{--                <h6 class="collapse-header">Other Pages:</h6>--}}
+{{--                <a class="collapse-item" href="404.html">404 Page</a>--}}
+{{--                <a class="collapse-item" href="blank.html">Blank Page</a>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </li>--}}
+
+    <!-- Nav Item - Charts -->
+{{--    <li class="nav-item">--}}
+{{--        <a class="nav-link" href="charts.html">--}}
+{{--            <i class="fas fa-fw fa-chart-area"></i>--}}
+{{--            <span>Charts</span></a>--}}
+{{--    </li>--}}
+
+    <!-- Nav Item - Tables -->
+{{--    <li class="nav-item">--}}
+{{--        <a class="nav-link" href="tables.html">--}}
+{{--            <i class="fas fa-fw fa-table"></i>--}}
+{{--            <span>Tables</span></a>--}}
+{{--    </li>--}}
+
+    <!-- Divider -->
+{{--    <hr class="sidebar-divider d-none d-md-block">--}}
+
+    <!-- Sidebar Toggler (Sidebar) -->
+{{--    <div class="text-center d-none d-md-inline">--}}
+{{--        <button class="rounded-circle border-0" id="sidebarToggle"></button>--}}
+{{--    </div>--}}
+
+</ul>

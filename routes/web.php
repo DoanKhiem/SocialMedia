23f930c995e9efbdd4532e7b0c6593db902d7fdd
khiemdev <khiembi161219@gmail.com>
2024-03-14 13:12:16 +0700

    setup admin theme

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes(['register' => false]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function () {
+    Route::get('/', [App\Http\Controllers\AdminController::class, 'admin'])->name('admin');
+
+});

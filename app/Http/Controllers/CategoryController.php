5532aaba1ddda9202e8df98f602a891469e954e5
doankhiem <khiembi161219@gmail.com>
2024-03-21 21:46:24 +0700

    admin show list category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
@@ -11,7 +12,8 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        //
+        $categories = Category::orderBy('id', 'DESC')->get();
+        return view('backend.categories.index', compact('categories'));
     }
 
     /**

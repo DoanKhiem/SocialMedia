1adf0dd3eb62c02c30b44b1a4ef5de6b7c07d652
khiemhome <khiembi161219@gmail.com>
2024-07-11 15:49:52 +0700

    Implement uploading avatar image on user

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ Route::get('/u/{user:username}', [ProfileController::class, 'index'])
     ->name('profile');
 Route::middleware('auth')->group(function () {
     Route::post('/profile/update-images', [ProfileController::class, 'updateImage'])
-        ->name('profile.updateCover');
+        ->name('profile.updateImages');
 //    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
 //    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
 //    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');

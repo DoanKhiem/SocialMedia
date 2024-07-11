e7dd57cdb9c8d676d3d858a2c18491d2d4a7a6f2
khiemhome <khiembi161219@gmail.com>
2024-07-11 12:51:28 +0700

    Implement user profile page UI

--- routes/web.php
+++ routes/web.php
@@ -15,16 +15,18 @@ use App\Http\Controllers\HomeController;
 //    ]);
 //})->middleware(['auth', 'verified']);
 
-    Route::get('/', [HomeController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/', [HomeController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
 
 //Route::get('/dashboard', function () {
 //    return Inertia::render('Dashboard');
 //})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+Route::get('/u/{user:username}', [ProfileController::class, 'index'])
+    ->name('profile');
+//Route::middleware('auth')->group(function () {
+//    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+//});
 
 require __DIR__.'/auth.php';

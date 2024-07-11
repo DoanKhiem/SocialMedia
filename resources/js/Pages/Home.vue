1cf0e7d0676de31b48cc0cb84711fc92d5604e52
khiemhome <khiembi161219@gmail.com>
2024-07-11 10:19:05 +0700

    Use AuthenticatedLayout on home to display navbar

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import { Head, Link } from '@inertiajs/vue3';
+import {Head} from '@inertiajs/vue3';
 import GroupList from "@/Components/app/GroupList.vue";
 import FollowingList from "@/Components/app/FollowingList.vue";
 import CreatePost from "@/Components/app/CreatePost.vue";
 import PostList from "@/Components/app/PostList.vue";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 
 defineProps<{
 
@@ -12,7 +13,7 @@ defineProps<{
 
 <template>
     <Head title="Social Media Website" />
-
+    <AuthenticatedLayout>
     <div class="grid lg:grid-cols-12 gap-3 p-4 h-full">
         <div class="lg:col-span-3 lg:order-1 h-full overflow-hidden">
             <GroupList />
@@ -25,4 +26,5 @@ defineProps<{
             <PostList class="flex-1"/>
         </div>
     </div>
+    </AuthenticatedLayout>
 </template>

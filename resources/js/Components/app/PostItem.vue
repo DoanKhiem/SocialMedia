1ff344110beed61d3ab6d36e32d88a57e9d610aa
khiemhome <khiembi161219@gmail.com>
2024-07-22 14:59:36 +0700

    Implement preview of the attachments

--- resources/js/Components/app/PostItem.vue
+++ resources/js/Components/app/PostItem.vue
@@ -1,16 +1,17 @@
 <script setup lang="ts">
 import {Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems} from '@headlessui/vue'
 import {EllipsisVerticalIcon, PencilIcon, TrashIcon} from '@heroicons/vue/20/solid'
-import {ChatBubbleLeftRightIcon, HandThumbUpIcon, ArrowDownTrayIcon} from '@heroicons/vue/24/outline'
+import {ArrowDownTrayIcon, ChatBubbleLeftRightIcon, HandThumbUpIcon} from '@heroicons/vue/24/outline'
 import PostUserHeader from "@/Components/app/PostUserHeader.vue";
-import { router } from '@inertiajs/vue3'
-import { isImage } from '@/helpers.ts'
+import {router} from '@inertiajs/vue3'
+import {isImage} from '@/helpers.ts'
+import {PaperClipIcon} from "@heroicons/vue/24/solid/index.js";
 
 const props = defineProps({
     post: Object
 })
 
-const emit = defineEmits(['editClick'])
+const emit = defineEmits(['editClick', 'attachmentClick'])
 
 function openEditModal() {
     emit('editClick', props.post)
@@ -22,6 +23,10 @@ function deletePost(){
             preserveScroll: true
         })
     }
+}
+
+function openAttachment(ind: any) {
+    emit('attachmentClick', props.post, ind)
 }
 </script>
 
@@ -90,9 +95,8 @@ function deletePost(){
             post.attachments.length === 1 ? 'grid-cols-1' : 'grid-cols-2'
         ]">
             <template v-for="(attachment, ind) of post.attachments.slice(0, 4)">
-
-                <div
-                    class="group aspect-square bg-blue-100 flex flex-col items-center justify-center text-gray-500 relative">
+                <div @click="openAttachment(ind)"
+                     class="group aspect-square bg-blue-100 flex flex-col items-center justify-center text-gray-500 relative cursor-pointer">
                     <div v-if="ind === 3 && post.attachments.length > 4"
                          class="absolute left-0 top-0 right-0 bottom-0 z-10 bg-black/60 text-white flex items-center justify-center text-2xl">
                         +{{ post.attachments.length - 4 }} more
@@ -107,17 +111,10 @@ function deletePost(){
                     <img v-if="isImage(attachment)"
                          :src="attachment.url"
                          class="object-contain aspect-square"/>
-                    <template v-else>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                             class="w-12 h-12">
-                            <path
-                                d="M5.625 1.5c-1.036 0-1.875.84-1.875 1.875v17.25c0 1.035.84 1.875 1.875 1.875h12.75c1.035 0 1.875-.84 1.875-1.875V12.75A3.75 3.75 0 0016.5 9h-1.875a1.875 1.875 0 01-1.875-1.875V5.25A3.75 3.75 0 009 1.5H5.625z"/>
-                            <path
-                                d="M12.971 1.816A5.23 5.23 0 0114.25 5.25v1.875c0 .207.168.375.375.375H16.5a5.23 5.23 0 013.434 1.279 9.768 9.768 0 00-6.963-6.963z"/>
-                        </svg>
-
+                    <div v-else class="flex flex-col justify-center items-center">
+                        <PaperClipIcon class="w-10 h-10 mb-3"/>
                         <small>{{ attachment.name }}</small>
-                    </template>
+                    </div>
                 </div>
             </template>
         </div>
--- resources/js/Components/app/PostList.vue
+++ resources/js/Components/app/PostList.vue
@@ -4,6 +4,7 @@ import PostItem from "@/Components/app/PostItem.vue";
 import PostModal from "@/Components/app/PostModal.vue";
 import {ref} from "vue";
 import {usePage} from "@inertiajs/vue3";
+import AttachmentPreviewModal from "@/Components/app/AttachmentPreviewModal.vue";
 
 defineProps({
     posts: Array
@@ -12,9 +13,18 @@ defineProps({
 const authUser = usePage().props.auth.user;
 const showEditModal = ref(false)
 const editPost = ref({})
+const showAttachmentsModal = ref(false)
+const previewAttachmentsPost = ref({})
 function openEditModal(post: object) {
     editPost.value = post;
     showEditModal.value = true;
+}
+function openAttachmentPreviewModal(post: any, index: number) {
+    previewAttachmentsPost.value = {
+        post,
+        index
+    }
+    showAttachmentsModal.value = true;
 }
 function onModalHide() {
     editPost.value = {
@@ -27,9 +37,14 @@ function onModalHide() {
 
 <template>
     <div class="overflow-auto">
-        <PostItem v-for="post of posts" :key="post.id" :post="post" @editClick="openEditModal"/>
-
+        <PostItem v-for="post of posts" :key="post.id" :post="post"
+                  @editClick="openEditModal"
+                  @attachmentClick="openAttachmentPreviewModal"
+        />
         <PostModal :post="editPost" v-model="showEditModal" @hide="onModalHide"/>
+        <AttachmentPreviewModal :attachments="previewAttachmentsPost.post?.attachments || []"
+                                v-model:index="previewAttachmentsPost.index"
+                                v-model="showAttachmentsModal"/>
     </div>
 </template>
 
